44d021c30feb878d57cd133ea962ce40f191e215
martypdx <marty@martypdx.com>
2021-02-11 12:34:49 -0800

    working calc with add and subtract

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// import functions and grab DOM elements
+const addendOne = document.getElementById('addend-one');
+const addendTwo = document.getElementById('addend-two');
+const addButton = document.getElementById('add-button');
+const sumDisplay = document.getElementById('sum-display');
 
-// initialize state
+const minuend = document.getElementById('minuend');
+const subtrahend = document.getElementById('subtrahend');
+const subtractButton = document.getElementById('subtract-button');
+const differenceDisplay = document.getElementById('difference-display');
 
-// set event listeners to update state and DOM
+addButton.addEventListener('click', () => {
+    const sum = Number(addendOne.value) + Number(addendTwo.value);
+    sumDisplay.value = sum;
+});
+
+subtractButton.addEventListener('click', () => {
+    const difference = Number(minuend.value) - Number(subtrahend.value);
+    differenceDisplay.value = difference;
+});
