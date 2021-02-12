c96c8df1d44976e3c0b90fd261da8d65a4062b1e
martypdx <marty@martypdx.com>
2021-02-11 16:08:21 -0800

    all tested calc functions done

--- test/example.test.js
+++ test/example.test.js
@@ -1,21 +1,60 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { add } from '../utilities.js';
+import { add, subtract, multiply, divide } from '../utilities.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const x = 4;
     const y = 5;
-    const sum = 9;
+    const expected = 9;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const result = add(x, y);
+    const actual = add(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(result, sum);
+    expect.equal(actual, expected);
+});
+
+test('subtract', (expect) => {
+    //Arrange
+    const x = 10;
+    const y = 2;
+    const expected = 8;
+    
+    //Act 
+    const actual = subtract(x, y);
+
+    //Expect
+    expect.equal(actual, expected);
+});
+
+test('multiply', (expect) => {
+    //Arrange
+    const x = 3;
+    const y = 4;
+    const expected = 12;
+    
+    //Act 
+    const actual = multiply(x, y);
+
+    //Expect
+    expect.equal(actual, expected);
+});
+
+test('divide', (expect) => {
+    //Arrange
+    const x = 12;
+    const y = 4;
+    const expected = 3;
+    
+    //Act 
+    const actual = divide(x, y);
+
+    //Expect
+    expect.equal(actual, expected);
 });
--- utilities.js
+++ utilities.js
@@ -3,3 +3,15 @@
 export function add(x, y) {
     return x + y;
 }
+
+export function subtract(x, y) {
+    return x - y;
+}
+
+export function multiply(x, y) {
+    return x * y;
+}
+
+export function divide(x, y) {
+    return x / y;
+}
